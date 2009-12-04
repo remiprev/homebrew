3cf3053d2e6ee7b60aaf5b46edd480cfa5ff7139
Justin Mazzi <hh@mailheist.com>
2009-12-04 16:21:22 -0500

    Adding vim Formula

---
+++ Library/Formula/vim.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Vim <Formula
+  url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.2.tar.bz2'
+  homepage 'http://www.vim.org'
+  md5 'f0901284b338e448bfd79ccca0041254'
+
+  def install
+		configure_args = [
+			"--prefix=#{prefix}",
+			"--disable-debug",
+			"--disable-dependency-tracking",
+		]
+
+		# Uses whatever is in your PATH
+		configure_args <<	"--enable-rubyinterp=/usr/bin/ruby" if ARGV.include? '--with-ruby'
+
+		system "./configure", *configure_args
+    system "make install"
+  end
+end
