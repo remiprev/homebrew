cc90bdb2035d68c167b64d126422ef81a46e6072
Justin Mazzi <hh@mailheist.com>
2009-12-06 13:54:29 -0500

    Disable gui

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -6,13 +6,17 @@ class Vim <Formula
   md5 'f0901284b338e448bfd79ccca0041254'
 
   def install
-		configure_args = [
-			"--prefix=#{prefix}",
-			"--disable-debug",
-			"--disable-dependency-tracking",
-		]
+    configure_args = [
+      "--prefix=#{prefix}",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--without-x",
+      "--disable-xim",
+      "--disable-fontset",
+      "--disable-xsmp",
+      "--enable-gui=no"
+    ]
 
-		# Uses whatever is in your PATH
 		configure_args <<	"--enable-rubyinterp=/usr/bin/ruby" if ARGV.include? '--with-ruby'
 
 		system "./configure", *configure_args
